c854335ab8ef7f5e186d62a6ae88ac72795c0768
Komal Shah <koms122@users.noreply.github.com>
2018-07-21 16:29:18 -0500

    Added hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    heroes: Hero[];
+    constructor( private heroService: HeroService ) { }
+    ngOnInit() {
+        this.getHeroes();
+    }
+    getHeroes(): void {
+        this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Superman' },
+    { id: 12, name: 'Batman' },
+    { id: 13, name: 'The Flash' },
+    { id: 14, name: 'Green Lantern' },
+    { id: 15, name: 'The Arrow' },
+    { id: 16, name: 'Wonder Woman' },
+    { id: 17, name: 'Mystique' },
+    { id: 18, name: 'Magneto' },
+    { id: 19, name: 'Deadpool' },
+    { id: 20, name: 'Black Canary' },
+];
